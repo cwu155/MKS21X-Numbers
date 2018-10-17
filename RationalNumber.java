ecad5f2b702a72d83f86f04caa512049f24a0d55
cwu155 <cwu17@stuy.edu>
2018-10-17 14:44:47 -0400

    Added DriverNumbers.java

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,31 +1,85 @@
 public class RationalNumber extends RealNumber{
   private int numerator, denominator;
 
-  public RationalNumber (int num, int den){
-    super (1.0);
-    numerator = num;
-    denominator = den;
+  public RationalNumber (int nume, int deno){
+    super (0.0);
+    numerator = nume;
+    denominator = deno;
+    if (deno == 0){
+      numerator = 0;
+      denominator = 1;
+    }
+    reduce();
+  }
+
+  public double getValue(){
+    return (0.0 + numerator ) / denominator;
   }
 
   public int getNumerator(){
-    return 0;
+    return numerator;
   }
 
   public int getDenominator(){
-    return 0;
+    return denominator;
   }
 
-  public int getRational(){
-    return 0;
-    // divides the numerator by denominator to get a rounded int value.
+  public RationalNumber reciprocal(){
+    return new RationalNumber(denominator, numerator);
   }
 
-  public boolean isZero(){
-    return false;
+  public boolean equals (RationalNumber other){
+    return (this.getValue() == other.getValue());
   }
 
   public String toString(){
-    return "";
+    if ((denominator == 1) || (numerator == 0)){
+      return (numerator + "");
+    } else {
+      return (numerator + "/" + denominator);
+    }
   }
+
+ private static int gcd(int a, int b){
+   if (b == 0){
+     return a;
+   } else {
+     return gcd(b, a % b);
+   }
+}
+
+ private void reduce(){
+   int GCD = gcd(numerator, denominator);
+   numerator = numerator / GCD;
+   denominator = denominator / GCD;
+ }
+
+ /******************Operations Return a new RationalNumber!!!!****************/
+ /**
+ *Return a new RationalNumber that is the product of this and the other
+ */
+ public RationalNumber multiply(RationalNumber other){
+   return new RationalNumber(this.getNumerator() * other.getNumerator(), this.getDenominator() * other.getDenominator());
+ }
+
+ /**
+ *Return a new RationalNumber that is the this divided by the other
+ */
+ public RationalNumber divide(RationalNumber other){
+    return new RationalNumber(this.getNumerator() * other.getDenominator(), this.getDenominator() * other.getNumerator());
+ }
+
+ /**
+ *Return a new RationalNumber that is the sum of this and the other
+ */
+ public RationalNumber add(RationalNumber other){
+   return new RationalNumber((this.getNumerator() * other.getDenominator() + this.getDenominator() * other.getNumerator()), (this.getDenominator() * other.getDenominator()));
+ }
+ /**
+ *Return a new RationalNumber that this minus the other
+ */
+ public RationalNumber subtract(RationalNumber other){
+   return new RationalNumber((this.getNumerator() * other.getDenominator() - this.getDenominator() * other.getNumerator()), (this.getDenominator() * other.getDenominator()));
+ }
 
 }
--- RealNumber.java
+++ RealNumber.java
@@ -1,14 +1,46 @@
 public class RealNumber{
   private double value;
 
-  public RealNumber (double newValue){
-     value = newValue;
-  }
-  public double getReal(){
-    return 0.0;
+  public RealNumber(double v){
+    value = v;
   }
 
+  public double getValue(){
+    return value;
+  }
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+     //other can be ANY RealNumber, including a RationalNumber
+     //or other subclasses of RealNumber (that aren't written yet)
+     return (this.value + other.getValue());
+  }
+
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return (this.value * other.getValue());
+  }
+
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return (this.value / other.getValue());
+  }
+
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return (this.value - other.getValue());
+  }
+
+
   public String toString(){
-      return "";
+    return "" + value;
   }
 }
